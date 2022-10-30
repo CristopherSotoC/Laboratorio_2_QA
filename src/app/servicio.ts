0e227e509182dca8909062387432a59b5bf60e94
CristopherSC <cristophersc2815956@estudiantec.cr>
2022-10-29 21:42:25 -0600

    first commit

---
+++ src/app/servicio.ts
@@ -0,0 +1,13 @@
+import { TipoServicio } from "./tipo-servicio.model";
+
+export interface Servicio {
+    getIdentificador():number;
+    setIdentificador(identificador:number):void;
+    getCosto():number;
+    setCosto(costo:number):void;
+    isPagado():boolean;
+    setPagado(pagado:boolean):void;
+    getTipo():TipoServicio;
+    setTipo(tipo:TipoServicio):void;
+    imprimir():string;
+}
---
+++ src/app/tipo-habitacion.ts
@@ -0,0 +1,7 @@
+export interface TipoHabitacion {
+    getNombre():string;
+    getDescripcion():string;
+    setNombre(n:string):void;
+    setDescripcion(d:string):void;
+    imprimir():string;
+}
